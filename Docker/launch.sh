9b2b376e06f1da517a7c0e35e0715a74d65f4b7b
Yuke Wang <505008605@qq.com>
2021-03-23 10:42:19 -0700

    update docker files

--- Docker/launch.sh
+++ Docker/launch.sh
@@ -1,2 +1,2 @@
-docker run -it -v $PWD/../:/GNNA osdi-ae:latest /bin/bash
+docker run -it --rm --gpus all -v $PWD/../:/GNNA osdi-ae:latest /bin/bash
 # docker run -it --rm --gpus all -v $PWD/../:/GNNA nvidia/cuda:11.2.2-cudnn8-devel-ubuntu18.04 /bin/bash

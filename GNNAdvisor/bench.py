a8ad39706cb83ee53d213fa9f30befd57333c31f
Yuke Wang <505008605@qq.com>
2021-03-25 14:35:59 -0700

    update with cu102 support

--- GNNAdvisor/bench.py
+++ GNNAdvisor/bench.py
@@ -2,16 +2,16 @@
 import os
 os.environ["PYTHONWARNINGS"] = "ignore"
 
-hidden = [64] 		# for GIN
-# hidden = [16] 			# for GCN
+# hidden = [64] 		# for GIN
+hidden = [16] 			# for GCN
 partsize_li = [32]  	# [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]:
 
 dataset = [
 		# ('tc_gnn_verify'	        , 96	    , 6   ),  
-		('citeseer'	        , 3703	    , 6   ),  
-		('cora' 	        , 1433	    , 7   ),  
-		('pubmed'	        , 500	    , 3   ),      
-		('ppi'	            , 50	    , 121 ),   
+		# ('citeseer'	        , 3703	    , 6   ),  
+		# ('cora' 	        , 1433	    , 7   ),  
+		# ('pubmed'	        , 500	    , 3   ),      
+		# ('ppi'	            , 50	    , 121 ),   
 
 		# ('PROTEINS_full'             , 29       , 2) ,   
 		# ('OVCAR-8H'                  , 66       , 2) , 
@@ -20,11 +20,11 @@ dataset = [
 		# ('TWITTER-Real-Graph-Partial', 1323     , 2) ,   
 		# ('SW-620H'                   , 66       , 2) ,
 		
-		# ( 'amazon0505'               , 96	, 22),
-		# ( 'artist'                   , 100  , 12),
-		# ( 'com-amazon'               , 96	, 22),
-		# ( 'soc-BlogCatalog'	       	 , 128  , 39), 
-		# ( 'amazon0601'  	         , 96	, 22), 
+		( 'amazon0505'               , 96	, 22),
+		( 'artist'                   , 100  , 12),
+		( 'com-amazon'               , 96	, 22),
+		( 'soc-BlogCatalog'	       	 , 128  , 39), 
+		( 'amazon0601'  	         , 96	, 22), 
 ]
 
 

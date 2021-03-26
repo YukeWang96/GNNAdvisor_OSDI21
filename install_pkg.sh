03db697ca29aa59c87487e15151236535dbb3030
Yuke Wang <505008605@qq.com>
2021-03-25 23:52:47 -0700

    update with install pkg

--- install_pkg.sh
+++ install_pkg.sh
@@ -1,7 +1,9 @@
 cd GNNAdvisor/GNNConv/
-python setup.py install
+TORCH_CUDA_ARCH_LIST="8.6" python setup.py install      # for RTX3090
+# TORCH_CUDA_ARCH_LIST="6.1" python setup.py install      # for Quadro P6000
 
 cd ../../
 
 cd rabbit_module/src/
-python setup.py install
+TORCH_CUDA_ARCH_LIST="8.6" python setup.py install      # for RTX3090
+# TORCH_CUDA_ARCH_LIST="6.1" python setup.py install      # for Quadro P6000

ccc4ba252dc8d7ad808949ed37457313fde9275b
Yuke Wang <505008605@qq.com>
2021-03-25 23:20:36 -0700

    local change on winnie

--- GNNAdvisor/bench.py
+++ GNNAdvisor/bench.py
@@ -13,18 +13,18 @@ dataset = [
 		# ('pubmed'	        , 500	    , 3   ),      
 		# ('ppi'	            , 50	    , 121 ),   
 
-		# ('PROTEINS_full'             , 29       , 2) ,   
-		# ('OVCAR-8H'                  , 66       , 2) , 
-		# ('Yeast'                     , 74       , 2) ,
-		# ('DD'                        , 89       , 2) ,
-		# ('TWITTER-Real-Graph-Partial', 1323     , 2) ,   
-		# ('SW-620H'                   , 66       , 2) ,
+		('PROTEINS_full'             , 29       , 2) ,   
+		('OVCAR-8H'                  , 66       , 2) , 
+		('Yeast'                     , 74       , 2) ,
+		('DD'                        , 89       , 2) ,
+		('TWITTER-Real-Graph-Partial', 1323     , 2) ,   
+		('SW-620H'                   , 66       , 2) ,
 		
-		( 'amazon0505'               , 96	, 22),
-		( 'artist'                   , 100  , 12),
-		( 'com-amazon'               , 96	, 22),
-		( 'soc-BlogCatalog'	       	 , 128  , 39), 
-		( 'amazon0601'  	         , 96	, 22), 
+		# ( 'amazon0505'               , 96	, 22),
+		# ( 'artist'                   , 100  , 12),
+		# ( 'com-amazon'               , 96	, 22),
+		# ( 'soc-BlogCatalog'	       	 , 128  , 39), 
+		# ( 'amazon0601'  	         , 96	, 22), 
 ]
 
 

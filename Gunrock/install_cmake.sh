2f549bdfde5e417fff48015e50604a3aac16bb78
Yuke Wang <505008605@qq.com>
2021-04-02 00:18:36 -0700

    update with README.md

---
+++ Gunrock/install_cmake.sh
@@ -0,0 +1,7 @@
+apt install build-essential libssl-dev
+wget https://github.com/Kitware/CMake/releases/download/v3.16.5/cmake-3.16.5.tar.gz
+tar -zxvf cmake-3.16.5.tar.gz
+cd cmake-3.16.5
+./bootstrap
+make 
+sudo make install 
